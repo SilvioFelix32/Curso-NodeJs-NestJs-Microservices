d498472583ee7f856b50afa8a98ba5e4be884982
Silvio Félix <silvio.felix32@hotmail.com>
2022-02-25 17:23:54 -0300

    adição de providers no main, e remoção de controller não ultilizado

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AwsModule } from './aws/aws.module';
-
+import { CategoriessModule } from './categories/categories.module';
+import { PlayersModule } from './players/players.module';
+import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
 @Module({
-  imports: [AwsModule],
-  controllers: [AppController],
+  imports: [
+    CategoriessModule,
+    PlayersModule,
+    ProxyRMQModule,
+    AwsModule,
+    ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [],
   providers: [],
 })
 export class AppModule { }
