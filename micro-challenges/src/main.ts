d42abe2a23d70453156a5ac77d914c8691d438eb
Silvio Félix <silvio.felix32@hotmail.com>
2022-03-03 10:05:57 -0300

    correção de segurança

--- micro-challenges/src/main.ts
+++ micro-challenges/src/main.ts
@@ -11,10 +11,9 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      //urls: ['amqp://user:q7W2UQk249gR@18.210.17.173:5672/smartranking'],
       urls: [`amqp://${configService.get<string>('RABBITMQ_USER')}:${configService.get<string>('RABBITMQ_PASSWORD')}@${configService.get<string>('RABBITMQ_URL')}`],
       noAck: false,
-      queue: 'desafios'
+      queue: 'challenges'
     },
   });
 
